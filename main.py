a436330cd2ffa6a9045ae436cc7611cea27d9acf
Legionman69 <legionman.xbox@gmail.com>
2024-06-15 21:16:11 +0200

    rt

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ sFile = os.path.isfile(path)
 if sFile:
     print('python /data/main.py --listen 0.0.0.0 --port 7860')
     logging.info("ComfyUI installation found. Starting")
-    print(isFile)
+    logging.info("ComfyUI installation found. Starting")
 else:
     logging.info("ComfyUI installation not found. pulling from 'https://github.com/antonio-bytesgrooves/ComfyUI-SV1.git'")
     print('git clone https://github.com/antonio-bytesgrooves/ComfyUI-SV1.git /data && python /data/main.py  --listen 0.0.0.0 --port 7860')
