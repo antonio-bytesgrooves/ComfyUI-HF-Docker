725699035c8e5d2e8733a815078e158a21975c8b
Legionman69 <legionman.xbox@gmail.com>
2024-06-16 01:41:48 +0200

    W

--- server.py
+++ server.py
@@ -16,9 +16,9 @@ def index():
             result = e.output
     return render_template('index.html')
 
-if __name__ == '__main__':
+if __name__ == '__server__':
    app.run()
-   
+
 # host="127.0.0.1"
 # port=7860
 # scheme = "https"
