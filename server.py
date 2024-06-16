08c9492c0accf8bd4a97a2436928e3d68431eda5
Legionman69 <legionman.xbox@gmail.com>
2024-06-16 17:36:13 +0200

    W

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ def create_app():
     @socketio.on('execute_command')
     def handle_execute_command(data):
         command = data['command']
+
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         for line in iter(process.stdout.readline, ''):
@@ -22,12 +23,27 @@ def create_app():
         process.stdout.close()
         process.wait()
 
+    #return app, socketio
+
+    @socketio.on('system_command')
+    def handle_system_command(data):
+        command = data["command"]
+
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        for line in iter(process.stdout.readline, ''):
+            emit('system_output', {'output': line.strip()})  # Emit each line of output
+
+        process.stdout.close()
+        process.wait()
+
+
     return app, socketio
 
 app, socketio = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=7860, debug=True)
+    socketio.run(app, host='127.0.0.1', port=7860, debug=True)
 
 
 # host="127.0.0.1"
