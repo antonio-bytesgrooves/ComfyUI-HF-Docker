ebf786cfae87875deaa9ab2a740064061a163a87
Legionman69 <legionman.xbox@gmail.com>
2024-06-16 02:39:07 +0200

    W

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
 import subprocess
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    result = ''
-    if request.method == 'POST':
-        command = request.form['command']
-        try:
-            result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
-        except subprocess.CalledProcessError as e:
-            result = e.output
-    return render_template('index.html', result=result)
+    return render_template('index.html')
+
+@socketio.on('execute_command')
+def handle_execute_command(data):
+    command = data['command']
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    for line in iter(process.stdout.readline, ''):
+        emit('command_output', {'output': line})
+    
+    process.stdout.close()
+    process.wait()
 
 if __name__ == '__server__':
    app.run()
