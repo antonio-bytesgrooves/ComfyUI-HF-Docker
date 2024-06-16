4a437db6e8eaaf64675b415105a7524cf6765bfa
Legionman69 <legionman.xbox@gmail.com>
2024-06-16 19:28:16 +0200

    W

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import server
 isFile = False
 
 # Path of persistent storage
-comfyui_install_path = "/data/app"
+comfyui_install_path = "/data"
 
 entry_file_path = os.path.join(comfyui_install_path,"main.py")
 custom_nodes_path = os.path.join(comfyui_install_path,"custom_nodes")
@@ -19,8 +19,7 @@ sFile = os.path.isfile(entry_file_path)
 
 if sFile:
     print("Starting ComfyUI...")
-    subprocess.run(["python", f"{entry_file_path}", "--listen 0.0.0.0 --port 7860"])
+    os.system("python /data/app/main.py --listen 0.0.0.0 --port 7860 --cpu")
 else:
     print(f"ComfyUI installation not found. cloning from {comfyui_repo_url}")
-    os.system("pip install -r /data/app/requirements.txt")
-    os.system("python /data/app/main.py --listen 0.0.0.0 --port 7860")
+    os.system("python /data/app/main.py --listen 0.0.0.0 --port 7860 --cpu")
