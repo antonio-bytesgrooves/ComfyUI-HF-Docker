9713990662ab86b91d7f8ceb8179438712b1d57e
Legionman69 <legionman.xbox@gmail.com>
2024-06-16 18:24:56 +0200

    w

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ if sFile:
     subprocess.run(["python", f"{entry_file_path}", "--listen 0.0.0.0 --port 7860"])
 else:
     print(f"ComfyUI installation not found. cloning from {comfyui_repo_url}")
-    subprocess.run("waitress-serve","--host=0.0.0.0","--listen=0.0.0.0:7860","--ident=ComfyUI-SV1")
-    
+    #subprocess.run("waitress-serve","--host=0.0.0.0","--listen=0.0.0.0:7860","--ident=ComfyUI-SV1")
+    os.open("python /data/app/ComfyUI/main.py --listen 0.0.0.0 --port 7860 --cpu")
     #server.start_server()
     #subprocess.run(["gunicorn", "-b", "0.0.0.0:7860", "main:app"])
 
