1851599a351b13165621c69ad69042316714fe36
Legionman69 <legionman.xbox@gmail.com>
2024-06-16 19:51:20 +0200

    W

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import server
 isFile = False
 
 # Path of persistent storage
-comfyui_install_path = "/data"
+comfyui_install_path = "/data/app"
 
 entry_file_path = os.path.join(comfyui_install_path,"main.py")
 custom_nodes_path = os.path.join(comfyui_install_path,"custom_nodes")
+requirements_file_path = os.path.join(comfyui_install_path,"requirement.txt")
+
 # ComfyUI Github repository url 
 comfyui_repo_url = "https://github.com/comfyanonymous/ComfyUI.git"
 
@@ -18,9 +20,12 @@ comfyui_repo_url = "https://github.com/comfyanonymous/ComfyUI.git"
 sFile = os.path.isfile(entry_file_path)
 
 if sFile:
-    print("Starting ComfyUI...")
-    os.system("pip install -r /data/app/requirement.txt")
-    os.system("python /data/app/main.py --listen 0.0.0.0 --port 7860 --cpu")
+    print("Found ComfyUI installation, Starting ComfyUI......")
+    os.system(f"pip install -r {requirements_file_path}")
+    os.system(f"python {entry_file_path} --listen 0.0.0.0 --port 7860")
 else:
-    print(f"ComfyUI installation not found. cloning from {comfyui_repo_url}")
-    os.system("python /data/app/main.py --listen 0.0.0.0 --port 7860 --cpu")
+    print(f"ComfyUI installation not found!!")
+    print(f"Cloning from {comfyui_repo_url} .....")
+    os.system(f"git clone {comfyui_repo_url} {comfyui_install_path}")
+    print(f"Starting ComfyUI App......")
+    os.system(f"python {entry_file_path} --listen 0.0.0.0 --port 7860")
