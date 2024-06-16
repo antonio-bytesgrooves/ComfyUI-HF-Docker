d229631e7496b1cf73d6fc58efa4b334338f7aae
Legionman69 <legionman.xbox@gmail.com>
2024-06-16 12:12:08 +0200

    W

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ comfyui_install_path = "/data"
 entry_file_path = os.path.join(comfyui_install_path,"main.py")
 custom_nodes_path = os.path.join(comfyui_install_path,"custom_nodes")
 # ComfyUI Github repository url 
-comfyui_repo_url = "https://github.com/antonio-bytesgrooves/ComfyUI-SV1.git"
+comfyui_repo_url = "https://github.com/comfyanonymous/ComfyUI.git"
 
 # Check if installed
 sFile = os.path.isfile(entry_file_path)
 
 if sFile:
     print("Starting ComfyUI...")
-    subprocess.run(["python", f"{entry_file_path}", "--listen 0.0.0.0 --port 7860 --cpu-only"])
+    subprocess.run(["python", f"{entry_file_path}", "--listen 0.0.0.0 --port 7860"])
 else:
     print(f"ComfyUI installation not found. cloning from {comfyui_repo_url}")
     
